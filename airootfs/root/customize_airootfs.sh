e1eba1d8251e517e739cfbd97a630d59ee63478c
isayni <lennysep@protonmail.com>
2022-07-09 23:49:32 +0200

    include vim plugins

---
+++ airootfs/root/customize_airootfs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# enabling sudo for wheel group
+echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+# installing vim plugins for user
+sudo -u user vim +PlugInstall +qall
+# installing coc extensions
+sudo -u user vim +"execute 'CocInstall -sync ' . join(g:coc_global_extensions, ' ')" +qall
+sudo -u user vim +CocUpdateSync +qall
